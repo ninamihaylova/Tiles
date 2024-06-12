1ba4bcd0e4efd453adb85c983ad2726b9e31047b
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-12 10:24:01 +0300

    Initial commit

---
+++ Tiles/Program.cs
@@ -0,0 +1,20 @@
+﻿class Tiles
+{
+    static void Main(string[] args)
+
+    {
+        double bathroomWidth = double.Parse(Console.ReadLine());
+        double bathroomHeight = double.Parse(Console.ReadLine());
+        double tileWidth = double.Parse(Console.ReadLine());
+        double tileHeight = double.Parse(Console.ReadLine());
+
+        
+        double bathroomArea = bathroomWidth * bathroomHeight;
+        double bathroomAreaWithSurplus = bathroomArea *1.10;
+        double tileArea = tileWidth * tileHeight;
+      
+        double tilesNeeded = bathroomAreaWithSurplus / tileArea;
+        Console.WriteLine($"{Math.Round(tilesNeeded,0)}");
+
+    }
+}
